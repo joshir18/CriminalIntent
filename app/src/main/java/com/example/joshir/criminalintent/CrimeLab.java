3edd942e1cea516f76aab733124bc66daf6bf1b4
joshir18 <joshir18@mail.wlu.edu>
2018-05-11 16:42:15 -0400

    added add crime, show subtitles and delete crime

--- app/src/main/java/com/example/joshir/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/joshir/criminalintent/CrimeLab.java
@@ -20,14 +20,17 @@ public class CrimeLab {
 
     private CrimeLab(Context context){
         mCrimes = new ArrayList<>();
-        for (int i = 0; i<100; i++){
-            Crime crime = new Crime();
-            crime.setTitle("Crime #"+i);
-            crime.setSolved(i % 2 == 0); //every other one
-            crime.setRequiresPolice(i % 3 == 0); //one in every 3
-            mCrimes.add(crime);
-        }
+
     }
+
+    public void addCrime (Crime c){
+        mCrimes.add(c);
+    }
+
+    public void deleteCrime (Crime c){
+        mCrimes.remove(c);
+    }
+
 
     public List<Crime> getCrimes() {
         return mCrimes;
